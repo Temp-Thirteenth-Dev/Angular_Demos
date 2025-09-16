708d1acaefba7c27e33f7c2d08d0d4c05e7e5eb9
Thirteenth Dev <thirteenth.dev@proton.me>
2025-09-16 18:23:43 +0530

    Ex 2A

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('Exp2');
+  validLogin = false;
+  InvalidLogin = false;
+  name ="User 1";
+  private pass = "Pass1";
+
+  validate(Uname:string , UPass:string){
+    if (this.name == Uname && this.pass == UPass ){
+      this.validLogin = true;
+      this.InvalidLogin = false;
+    }
+    else{
+      this.validLogin = false;
+      this.InvalidLogin = true;
+    }
+  }
 }
