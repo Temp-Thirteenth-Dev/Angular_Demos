8accdbc5b16a9a54f0a89cf0081966865d87aeb0
Thirteenth Dev <thirteenth.dev@proton.me>
2025-09-16 23:01:13 +0530

    Fixing Parameter problem for Repeat() by passing Mock Objects

--- src/app/repeat.spec.ts
+++ src/app/repeat.spec.ts
@@ -2,7 +2,9 @@ import { Repeat } from "./repeat";
 
 describe('Repeat', () => {
   it('should create an instance', () => {
-    const directive = new Repeat();
+    const mockTemplateRef = {} as any;
+    const mockViewContainerRef = {} as any;
+    const directive = new Repeat(mockTemplateRef, mockViewContainerRef);
     expect(directive).toBeTruthy();
   });
 });
