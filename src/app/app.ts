21cd53162865ee8255092c0e0e070bb09c53fe6b
Thirteenth Dev <thirteenth.dev@proton.me>
2025-09-17 18:49:15 +0530

    5A

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LowerCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LowerCasePipe,UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Exp2');
+  protected readonly title = signal('pipes');
+  showVar=false;
+  code='Prd_Code_1234';
+  name='The Product';
+  show() {
+    if(this.showVar==false){
+      this.showVar=true;
+    }
+    else{
+      this.showVar=false;
+    }
+  }
 }
