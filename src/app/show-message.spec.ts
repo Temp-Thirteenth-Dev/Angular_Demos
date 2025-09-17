dc8b08b8e33970b4294098a5eeb316f48c0e2975
Thirteenth Dev <thirteenth.dev@proton.me>
2025-09-17 17:51:43 +0530

    Ex 3C with Mock objects in show-message.spces.ts file

--- src/app/show-message.spec.ts
+++ src/app/show-message.spec.ts
@@ -2,7 +2,13 @@ import { ShowMessage } from './show-message';
 
 describe('ShowMessage', () => {
   it('should create an instance', () => {
-    const directive = new ShowMessage();
+    const mockElementRef = { nativeElement: {} } as any;
+    const mockRenderer2 = {
+      setStyle: () => {},
+      removeStyle: () => {},
+      // add other methods if needed
+    } as any;
+    const directive = new ShowMessage(mockElementRef, mockRenderer2);
     expect(directive).toBeTruthy();
   });
 });
