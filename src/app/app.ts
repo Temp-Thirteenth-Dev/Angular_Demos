a9e12104c5f79d5d3a9c44360897de117043e500
Thirteenth Dev <thirteenth.dev@proton.me>
2025-09-19 00:12:11 +0530

    Simple 5b

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LowerCasePipe, UpperCasePipe ,DatePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LowerCasePipe,UpperCasePipe , DatePipe , CurrencyPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Exp2');
+  protected readonly title = signal('pipes');
+  showVar=false;
+  code='Prd_Code_1234';
+  name='The Product';
+  price = 3500;
+  mfg_date = '2025-04-25';
+  show() {
+    if(this.showVar==false){
+      this.showVar=true;
+    }
+    else{
+      this.showVar=false;
+    }
+  }
 }
