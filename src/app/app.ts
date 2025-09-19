397932284cfe9a98333a62e324cc7a8fbdc6f19a
Thirteenth Dev <thirteenth.dev@proton.me>
2025-09-19 20:37:47 +0530

    5C

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseList } from './course-list/course-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , CourseList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Exp2');
+  showVar = false;
+    show() {
+    if(this.showVar==false){
+      this.showVar=true;
+    }
+    else{
+      this.showVar=false;
+    }
+  }
 }
--- src/app/course-list/course-list.ts
+++ src/app/course-list/course-list.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './course-list.css'
 })
 export class CourseList {
-
+  courses=['Information Security','Quantum Technology','Artificial Super Intelligence'];
 }
