a4fe40279c5100720cd2422aa6b950dfa12fbc0b
Thirteenth Dev <thirteenth.dev@proton.me>
2025-09-19 23:52:23 +0530

    Event Binding Done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Exp2');
+  name = "Enter Your Name";
 }
