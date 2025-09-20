64567a1495d3d779192bbef8fe25e3faee102106
Thirteenth Dev <thirteenth.dev@proton.me>
2025-09-20 10:34:38 +0530

    6B cpu0506-like

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseList } from './course-list/course-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CourseList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Exp2');
+  protected readonly title = signal('components');
+  sub_name='';
+  registered_courses:string[]=[];
+  submit(name:string){
+    this.sub_name=name;
+  }
+  receive_registration($event:string){
+    this.registered_courses.push($event);
+  }
 }
--- src/app/course-list/course-list.ts
+++ src/app/course-list/course-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-course-list',
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './course-list.css'
 })
 export class CourseList {
-
+    @Input() course_name!:string;
+    @Output() registered_course_emitter=new EventEmitter<string>();
+    course_details=[
+        {"id":"sub-1","name":"course-1",'branch':'b1'},
+        {"id":"sub-2","name":"course-2",'branch':'b1'},
+        {"id":"sub-3","name":"course-3",'branch':'b2'},
+        {"id":"sub-4","name":"course-4",'branch':'b2'}
+    ]
+    send_registration(){
+        this.registered_course_emitter.emit(this.course_name);
+    }
 }
