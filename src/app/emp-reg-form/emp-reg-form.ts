b74b7cb302568eb1fa210b0d4ffec2c0cf602602
Thirteenth Dev <thirteenth.dev@proton.me>
2025-10-07 17:43:27 +0530

    simple validations

--- src/app/emp-reg-form/emp-reg-form.ts
+++ src/app/emp-reg-form/emp-reg-form.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-emp-reg-form',
@@ -10,9 +10,9 @@ import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
 export class EmpRegForm {
   // frmcntrl = new FormControl(''); for just one field, but not recommended for many fields.
   empform = new FormGroup({
-    firstName: new FormControl(''),
+    firstName: new FormControl('', Validators.required),
     lastName: new FormControl(''),
-    email: new FormControl('')
+    email: new FormControl('', [Validators.required, Validators.email])
   });
 
   submitted = false;
