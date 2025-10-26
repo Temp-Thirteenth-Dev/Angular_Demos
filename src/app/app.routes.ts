d7d80a2855f8490bafe5269d9354946b8a6f6823
Thirteenth Dev <thirteenth.dev@proton.me>
2025-10-26 14:45:50 +0530

    10A

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Book } from './book/book';
+import { User } from './user/user';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:'',
+        component : Home,
+    },
+    {
+        path : 'user',
+        component:User,
+    },
+    {
+        path:'book',
+        component:Book,
+    }
+];
